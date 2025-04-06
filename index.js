27c8feae655a72c1e034a65aa9212cf4df598be4
Saharsh Patil <saharshap3@gmail.com>
2025-04-06 21:19:22 +0530

    add calculator

--- index.js
+++ index.js
@@ -4,32 +4,42 @@ const port = 3000;
 
 // 🔁 Counter variable that increases with every request
 let requestCount = 0;
-const currentTime = new Date()
+const currentTime = new Date();
 
-app.get("/", (req, res) => {
+app.get("/counter", (req, res) => {
   requestCount++; // Increment the counter
   res.send(`${requestCount}`);
-  console.log(`${currentTime}`)
+  console.log(`${currentTime}`);
 });
 
-
 app.get("/calculate", (req, res) => {
-  let num1 = Number(req.headers.num1)
-  let num2 = Number(req.headers.num2)
-  let operation = req.headers.operation
-  let Result = 0
+  let num1 = Number(req.headers.num1);
+  let num2 = Number(req.headers.num2);
+  let operation = req.headers.operation;
+  let Result = 0;
 
-  if(operation == 'Add'){
-    Result = num1 + num2
-  res.send(`${Result}`);
-    
-  }else{
-    res.send(`Invalid request`);
-    console.log(req.headers)
+  if (operation == "Addition") {
+    Result = num1 + num2;
+    res.send(`${Result}`);
   }
-  
-  
-  console.log(`${currentTime}`)
+  if (operation == "Substraction") {
+    Result = num1 - num2;
+    res.send(`${Result}`);
+
+  }
+  if (operation == "Multiplication") {
+    Result = num1 * num2;
+    res.send(`${Result}`);
+  }
+  if (operation == "Divison") {
+    Result = num1 / num2;
+    res.send(`${Result}`);
+  } else {
+    res.send(`Invalid request`);
+    console.log(req.headers);
+  }
+
+  console.log(`${currentTime}`);
 });
 
 app.listen(port, () => {
