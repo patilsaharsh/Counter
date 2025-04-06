94fdb94b39711a9ad3198196c4bec2da65912bc6
Saharsh Patil <saharshap3@gmail.com>
2025-04-06 19:56:43 +0530

    Added time with every req

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const port = 3000;
 
 // 🔁 Counter variable that increases with every request
 let requestCount = 0;
+const currentTime = new Date()
 
 app.get("/", (req, res) => {
   requestCount++; // Increment the counter
   res.send(`${requestCount}`);
+  console.log(`${currentTime}`)
 });
 
 app.listen(port, () => {
